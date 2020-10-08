34ea24c67ea6d5ebf20dfaf388e11835c12d9574
Akos Becsey <becsey.a@gmail.com>
2020-10-08 22:11:47 +0200

    Bitmap

---
+++ bitmap.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <cstring>
+
+class Bitmap {
+protected:
+    static const int MAX_WIDTH = 25;
+    static const int MAX_HEIGHT = 20;
+
+    int width;
+    int height;
+    char color;
+    char buffer[MAX_WIDTH * MAX_HEIGHT];
+
+public:
+    Bitmap(int width, int height, char color = 'X', const char* data = nullptr)
+        : width(width)
+        , height(height)
+        , color(color)
+    {
+        memset(buffer, 0, MAX_WIDTH * MAX_HEIGHT);
+
+        if (data)
+            for (int i = 0; i < width*height; i++)
+                buffer[i] = data[i];
+    }
+
+    const int getWidth() const {
+        return width;
+    }
+
+    const int getHeight() const {
+        return height;
+    }
+
+    char get(int x, int y) const {
+        return buffer[width*y + x];
+    }
+
+    char getC(int x, int y) const {
+        return (buffer[width*y + x] ? buffer[width*y + x] : ' ');
+    }
+
+    void inc(int x, int y) {
+        buffer[width*y + x]++;
+    }
+
+    void set(int x, int y, char val) {
+        buffer[width*y + x] = val;
+    }
+
+    void setColor(int color) {
+        this->color = color;
+    }
+
+    void clear() {
+        for (int i = 0; i < width*height; i++)
+            buffer[i] = 0;
+    }
+
+    void draw(const Bitmap& other, int x0, int y0) {
+        for (int y = y0; (y < height && (y-y0) < other.height); y++)
+            for (int x = x0; (x < width && (x-x0) < other.width); x++)
+                if (other.get(x-x0,y-y0))
+                    inc(x ,y);
+    }
+
+    void drawColor(const Bitmap& other, int x0, int y0) {
+        for (int y = y0; (y < height && (y-y0) < other.height); y++)
+            for (int x = x0; (x < width && (x-x0) < other.width); x++)
+                if (other.get(x-x0,y-y0))
+                    set(x, y, other.color);
+    }
+
+    void undraw(const Bitmap& other, int x0, int y0) {
+        for (int y = y0; (y < height && (y-y0) < other.height); y++)
+            for (int x = x0; (x < width && (x-x0) < other.width); x++)
+                if (other.get(x-x0,y-y0))
+                    set(x, y, 0);
+    }
+
+    Bitmap rotate() const {
+        Bitmap r(height, width, color);
+
+        for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++)
+                r.set(y, x, get((width-1)-x, y));
+
+        return r;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
 #include <stdio.h>
 
+#include "bitmap.hpp"
+#include <vector>
+
+Bitmap canvas(25,5);
+
+const std::vector<Bitmap> shapes{
+    {5, 1, 'A', (const char[]) {1, 1, 1, 1, 1}},
+    {4, 2, 'B', (const char[]) {0, 1, 1, 1, 1, 1, 0, 0,}},
+    {4, 2, 'C', (const char[]) {1, 1, 1, 1, 1, 0, 0, 0,}},
+    {4, 2, 'D', (const char[]) {1, 1, 1, 1, 0, 1, 0, 0,}},
+    {3, 3, 'E', (const char[]) {1, 1, 1, 0, 1, 0, 0, 1, 0}},
+    {3, 3, 'F', (const char[]) {0, 1, 0, 1, 1, 1, 0, 1, 0}},
+    {3, 3, 'G', (const char[]) {1, 1, 0, 0, 1, 0, 0, 1, 1}},
+    {3, 3, 'H', (const char[]) {1, 1, 1, 0, 0, 1, 0, 0, 1}},
+    {3, 3, 'I', (const char[]) {1, 1, 0, 0, 1, 1, 0, 1, 0}},
+    {3, 3, 'J', (const char[]) {1, 1, 0, 0, 1, 1, 0, 0, 1}},
+    {3, 2, 'K', (const char[]) {1, 0, 1, 1, 1, 1}},
+    {3, 2, 'L', (const char[]) {1, 1, 1, 1, 1, 0}},
+};
+
+void print_bitmap(const Bitmap& bitmap) {
+    for (int y = 0; y < bitmap.getHeight(); y++) {
+        for (int x = 0; x < bitmap.getWidth(); x++)
+            printf("%c", bitmap.getC(x,y));
+        printf("\n");
+    }
+}
 
 int main() {
-    printf("Hello word\n");
+    for (size_t i = 0; i < shapes.size(); i++) {
+        Bitmap b = shapes[i];
+        for (size_t j = 0; j < 4; j++)
+        {
+            canvas.drawColor(b, j*6, 0);
+            b = b.rotate();
+        }
+        print_bitmap(canvas);
+        canvas.clear();
+        printf("---\n");
+    }
 }
