f2d8cde583aa956161df8db8a7984a83842da41a
Akos Becsey <becsey.a@gmail.com>
2020-10-12 16:38:27 +0200

    Renamed shapes

--- main.cpp
+++ main.cpp
@@ -5,18 +5,18 @@
 
 constexpr size_t SHAPE_NUM = 12;
 const Shape shapes_arr[SHAPE_NUM]{
-    (Bitmap) {3, 3, 'E', (const char[]) {1, 1, 1, 0, 1, 0, 0, 1, 0}},
-    (Bitmap) {3, 3, 'F', (const char[]) {0, 1, 0, 1, 1, 1, 0, 1, 0}},
-    (Bitmap) {3, 3, 'G', (const char[]) {1, 1, 0, 0, 1, 0, 0, 1, 1}},
-    (Bitmap) {3, 3, 'H', (const char[]) {1, 1, 1, 0, 0, 1, 0, 0, 1}},
-    (Bitmap) {3, 3, 'I', (const char[]) {1, 1, 0, 0, 1, 1, 0, 1, 0}},
-    (Bitmap) {3, 3, 'J', (const char[]) {1, 1, 0, 0, 1, 1, 0, 0, 1}},
-    (Bitmap) {3, 2, 'K', (const char[]) {1, 0, 1, 1, 1, 1}},
-    (Bitmap) {3, 2, 'L', (const char[]) {1, 1, 1, 1, 1, 0}},
-    (Bitmap) {4, 2, 'B', (const char[]) {0, 1, 1, 1, 1, 1, 0, 0,}},
-    (Bitmap) {4, 2, 'C', (const char[]) {1, 1, 1, 1, 1, 0, 0, 0,}},
-    (Bitmap) {4, 2, 'D', (const char[]) {1, 1, 1, 1, 0, 1, 0, 0,}},
-    (Bitmap) {5, 1, 'A', (const char[]) {1, 1, 1, 1, 1}},
+    (Bitmap) {3, 3, 'X', (const char[]) {0, 1, 0, 1, 1, 1, 0, 1, 0}},
+    (Bitmap) {3, 3, 'T', (const char[]) {1, 1, 1, 0, 1, 0, 0, 1, 0}},
+    (Bitmap) {3, 3, 'Z', (const char[]) {1, 1, 0, 0, 1, 0, 0, 1, 1}},
+    (Bitmap) {3, 3, 'V', (const char[]) {1, 1, 1, 0, 0, 1, 0, 0, 1}},
+    (Bitmap) {3, 3, 'F', (const char[]) {1, 1, 0, 0, 1, 1, 0, 1, 0}},
+    (Bitmap) {3, 3, 'W', (const char[]) {1, 1, 0, 0, 1, 1, 0, 0, 1}},
+    (Bitmap) {3, 2, 'U', (const char[]) {1, 0, 1, 1, 1, 1}},
+    (Bitmap) {3, 2, 'P', (const char[]) {1, 1, 1, 1, 1, 0}},
+    (Bitmap) {4, 2, 'N', (const char[]) {0, 1, 1, 1, 1, 1, 0, 0,}},
+    (Bitmap) {4, 2, 'Y', (const char[]) {1, 1, 1, 1, 0, 1, 0, 0,}},
+    (Bitmap) {4, 2, 'L', (const char[]) {1, 1, 1, 1, 1, 0, 0, 0,}},
+    (Bitmap) {5, 1, 'I', (const char[]) {1, 1, 1, 1, 1}},
 };
 ConstFastVector<Shape> shapes(shapes_arr, SHAPE_NUM);
 
